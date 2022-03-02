3c985e5e3a29102e1c408a1bc860b3434e9b807c
toljaEmbroker <aleksa.toljic@embroker.com>
2022-03-02 21:47:04 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Table} from '@react-to-styled/table';
+import {EssentialStyle} from '@react-to-styled/essentials'
+export const columns = {
+  title: {
+    header: 'Title',
+    Cell: ({ data: { title } }) => <span>{title}</span>,
+  },
+  price: {
+    header: 'Price',
+    Cell: ({ data: { price } }) => <span>{price}</span>,
+  },
+};
 
+export const data = [
+  { title: 'One', price: 100 },
+  { title: 'Two', price: 200 },
+];
 function App() {
   return (
     <div className="App">
+      <EssentialStyle/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +36,7 @@ function App() {
           Learn React
         </a>
       </header>
+<Table columns={columns} data={data} />
     </div>
   );
 }
