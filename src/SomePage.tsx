22c9a6c6bc9d1002d33619856a7e0ab4d1111baa
aleksaToljic <aleksa.toljic@gmail.com>
2022-05-03 21:06:35 +0200

    Using components on one page with typescript

--- src/SomePage.tsx
+++ src/SomePage.tsx
@@ -1,11 +1,11 @@
-import { Icon } from "@react-to-styled/icon";
-import { Loader } from "@react-to-styled/loader";
-import { Toggle } from "@react-to-styled/toggle";
 import React from "react";
-import logo from "./logo.svg";
+import {
+  Icon,
+  Loader,
+  Table,
+  EssentialStyle,
+} from "@react-to-styled/components";
 import "./App.css";
-import { Table } from "@react-to-styled/table";
-import { EssentialStyle } from "@react-to-styled/essentials";
 export const columns = {
   title: {
     header: "Title",
@@ -21,16 +21,15 @@ export const data = [
   { title: "One", price: 100 },
   { title: "Two", price: 200 },
 ];
-function App() {
+function SomePage() {
   return (
     <div className="App">
       <EssentialStyle />
       <Icon name="arrow_down" />
-      <Toggle />
       <Loader loaderSize={30} wrapperHeight={300} />
       <Table columns={columns} data={data} />
     </div>
   );
 }
 
-export default App;
+export default SomePage;
